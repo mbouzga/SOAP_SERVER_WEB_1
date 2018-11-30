76d21522c0af1c6504c9ac7b004f8c81a7a1f122
MountassirBOUZGA <MountassirBOUZGA@DESKTOP-O4BMO5M.lille.fr.ibm.com>
2018-11-30 16:20:20 +0100

    Test WEB service avec Spring boot: Tuto : https://spring.io/guides/gs/producing-web-service/

--- src/main/java/com/soap/run/SimulServer.java
+++ src/main/java/com/soap/run/SimulServer.java
@@ -6,10 +6,10 @@ import com.soap.services.impl.Compte;
 
 public class SimulServer {
 
-	public static void main(String[] args) {
-		String url = "http://localhost:1987/";
-		Endpoint.publish(url, new Compte());
-		System.out.println("Publication service OK ...");
-	}
+//	public static void main(String[] args) {
+//		String url = "http://localhost:1987/";
+//		Endpoint.publish(url, new Compte());
+//		System.out.println("Publication service OK ...");
+//	}
 
 }
