8fa5d0dc41478bb51784fb2fade6a3d95e308471
MountassirBOUZGA <MountassirBOUZGA@DESKTOP-O4BMO5M.lille.fr.ibm.com>
2018-12-03 16:48:16 +0100

    Test SOAPUI/WSDL

--- src/main/java/com/soap/configuration/WebServiceConfig.java
+++ src/main/java/com/soap/configuration/WebServiceConfig.java
@@ -28,7 +28,7 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
 		wsdl11Definition.setPortTypeName("CountriesPort");
 		wsdl11Definition.setLocationUri("/ws");
-		wsdl11Definition.setTargetNamespace("beans.soap.com");
+		wsdl11Definition.setTargetNamespace("bouzga");
 		wsdl11Definition.setSchema(countriesSchema);
 		return wsdl11Definition;
 	}
