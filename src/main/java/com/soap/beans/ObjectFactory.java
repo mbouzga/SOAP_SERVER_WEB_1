0840207514b860b2fa286b8d250005e60b9132d1
MountassirBOUZGA <MountassirBOUZGA@DESKTOP-O4BMO5M.lille.fr.ibm.com>
2018-12-03 16:48:32 +0100

    Test SOAPUI/WSDL

--- src/main/java/com/soap/beans/ObjectFactory.java
+++ src/main/java/com/soap/beans/ObjectFactory.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2018.12.03 à 04:17:01 PM CET 
+// Généré le : 2018.12.03 à 04:42:38 PM CET 
 //
 
 
